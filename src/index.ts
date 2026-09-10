208f9545f19030f42ffc0283bbbd4296b587e785
Pavel <pashoktver90@gmail.com>
2026-09-10 15:27:29 +0300

    add vercel config

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { setupApp } from "./setup-app";
 const app = express();
 setupApp(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server is running");
